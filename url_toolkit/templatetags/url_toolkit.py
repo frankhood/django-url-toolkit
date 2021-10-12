53417152633ab7cb5e15803bc7a8edc7cb022456
Antonio Mustich <a.mustich@frankhood.it>
2021-10-12 18:42:55 +0200

    Fix on setup.py

--- url_toolkit/templatetags/url_toolkit.py
+++ url_toolkit/templatetags/url_toolkit.py
@@ -1,27 +1,26 @@
 from django import template
-# from django.contrib.staticfiles.templatetags.staticfiles import static
-# from django.templatetags.static import StaticNode
+from django.templatetags.static import StaticNode, static
 
 from url_toolkit.utils import make_absolute_url
 
 register = template.Library()
 
-#
-# class StaticFilesAbsoluteNode(StaticNode):
-#
-#     def url(self, context):
-#         path = self.path.resolve(context)
-#         static_url = static(path)
-#         return make_absolute_url(static_url)
-#
-#
-# @register.tag('static_absolute')
-# def do_static_absolute(parser, token):
-#     """
-#     '{% load url_toolkit %}'
-#     "{% static_absolute 'test/fb.png' %}"
-#     """
-#     return StaticFilesAbsoluteNode.handle_token(parser, token)
+
+class StaticFilesAbsoluteNode(StaticNode):
+
+    def url(self, context):
+        path = self.path.resolve(context)
+        static_url = static(path)
+        return make_absolute_url(static_url)
+
+
+@register.tag('static_absolute')
+def do_static_absolute(parser, token):
+    """
+    '{% load url_toolkit %}'
+    "{% static_absolute 'test/fb.png' %}"
+    """
+    return StaticFilesAbsoluteNode.handle_token(parser, token)
 
 
 @register.filter
